bd347ba57233abcf321ca83ec6b1cde398091a9d
deepaksoam313 <deepaksoam313@gmail.com>
2025-02-14 02:45:37 +0530

    Initial commit

---
+++ src/main/java/com/deepak/springsecsection1/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.deepak.springsecsection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to the Spring Application with security";
+    }
+}
